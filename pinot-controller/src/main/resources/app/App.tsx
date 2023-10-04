c59cfd70ca84deaa531bd8b2951370ed40e10fc6
Erich Ess <erich@startree.ai>
2023-10-03 17:16:37 -0700

    Revert "Intercom script addition in StarTree Pinot"
    This reverts commit 7986c9524526f49ab1d679a18fddcfda8c276c0b.

--- pinot-controller/src/main/resources/app/App.tsx
+++ pinot-controller/src/main/resources/app/App.tsx
@@ -39,7 +39,7 @@ export const App = () => {
     // authentication already handled by authProvider
     if (authUserEmail && authenticated) {
       // Authenticated with an auth method that supports user identity
-      let scriptToInsert="(function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/bxs433fe';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();window.Intercom('boot',{api_base:'https://api-iam.intercom.io',app_id:'bxs433fe',email:'"+authUserEmail+"',name:'"+authUserName+"',user_id:'"+authUserEmail+"}'})";if(document.head){const scriptTag=document.createElement('script');scriptTag.innerHTML=scriptToInsert;scriptTag.async=true;document.head.appendChild(scriptTag);}
+      // Any code that needs user identity can go here
     }
 
     if (authenticated) {
